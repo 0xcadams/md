99b45bb8d393676080a85bfc29291caf5e8d6881
Chase Adams <c@cadams.io>
2026-07-16 10:27:57 -0700

    fix: expose Hono import to Vercel

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import {fileURLToPath} from "node:url";
 
+import {Hono} from "hono";
+
 import {createApp} from "./src/app";
 
-const app = await createApp({
+const app: Hono = await createApp({
   assetDirectory: fileURLToPath(new URL("./public/assets/", import.meta.url)),
   root: fileURLToPath(new URL("./demo-files/", import.meta.url)),
 });
